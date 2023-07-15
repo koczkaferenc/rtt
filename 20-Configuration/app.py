b5a201d077f6676944422fc0c3ab0a6dac9a75a4
koczkaferenc <info@linux-szerver.hu>
2023-07-15 09:14:19 +0200

    Additional developments

---
+++ 20-Configuration/app.py
@@ -0,0 +1,18 @@
+'''
+Configuration example
+Based on http://exploreflask.com/en/latest/configuration.html
+The config file and app.py must be placed in the same directory.
+'''
+from flask import Flask
+
+# Load our own configuration: instance_relative_config=True
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+def index():
+    return 'Hello, the name of the database is: ' + app.config['DATABASE_NAME'] + ', the first user is: ' + app.config['USERS'][0]
+
+
+app.run(host=app.config["SERVER_ADDRESS"], port=app.config["SERVER_PORT"])
---
+++ 20-Configuration/config.py
@@ -0,0 +1,9 @@
+DEBUG = True
+
+#
+SERVER_ADDRESS = "127.0.0.1"
+SERVER_PORT = "5001"
+
+#
+DATABASE_NAME = "VeryImportantDb"
+USERS = ['koczka.ferenc', 'kiss.lajos']
